a9a6945f4ef543f9db613afb952fd450d19352bb
petrarka51 <petrarka51@gmail.com>
2025-05-23 17:26:43 +0300

    Update Semen.tsx

--- src/components/Semen.tsx
+++ src/components/Semen.tsx
@@ -24,10 +24,12 @@ const Semen: React.FC<SemenProps> = ({ className = "" }) => {
   return (
     <div className={flex flex-col items-center ${className}}>
       <div className="relative">
-        <svg 
-          className="absolute top-0 right-0 transform translate-x-12 -translate-y-8 w-36 h-36 text-gray-100"
-          viewBox="0 0 100 100" 
-          xmlns="http://www.w3.org/2000/svg"
+        <img
+          src="/image_semen_main"
+          width="120" 
+          height="120" 
+          viewBox="0 0 120 120" 
+          className="transform -scale-x-100"
         >
           <path d="M50,5 Q80,20 80,50 Q80,80 50,95 Q20,80 20,50 Q20,20 50,5" fill="#f9fafb" />
         </svg>
